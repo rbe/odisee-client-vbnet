bdb2d3076076664ef11ae2db8c79b2218300a11d
Ralf Bensmann <ralf@art-of-coding.eu>
2012-12-02 20:10:42 +0100

    Changed version number to 2.2.3.17 (forgot .16)

--- OdiseeClient/OdiseeClient/My Project/AssemblyInfo.vb
+++ OdiseeClient/OdiseeClient/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' übernehmen, indem Sie "*" eingeben:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.2.3.15")> 
-<Assembly: AssemblyFileVersion("2.2.3.15")> 
+<Assembly: AssemblyVersion("2.2.3.17")> 
+<Assembly: AssemblyFileVersion("2.2.3.17")> 
